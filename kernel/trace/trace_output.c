3881368b2beea0c2cad04ecb890a1d29279b6d67
HacKurx <root@opensec.fr>
2018-12-02 11:31:20 +0100

    fixed some more constification fallout
    From: PaX Team

--- kernel/trace/trace_output.c
+++ kernel/trace/trace_output.c
@@ -824,6 +824,7 @@ int register_ftrace_event(struct trace_event *event)
 			goto out;
 	}
 
+	pax_open_kernel();
 	if (event->funcs->trace == NULL)
 		event->funcs->trace = trace_nop_print;
 	if (event->funcs->raw == NULL)
@@ -832,6 +833,7 @@ int register_ftrace_event(struct trace_event *event)
 		event->funcs->hex = trace_nop_print;
 	if (event->funcs->binary == NULL)
 		event->funcs->binary = trace_nop_print;
+	pax_close_kernel();
 
 	key = event->type & (EVENT_HASHSIZE - 1);
 
